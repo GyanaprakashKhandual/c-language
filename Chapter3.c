8c6613efbc25d92072d8311ed7b6375dffd6897f
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-06-30 20:36:30 +0530

    Day 1 Complete

---
+++ Chapter3.c
@@ -0,0 +1,69 @@
+// Conditional Statements
+
+// If Else statements
+
+/*
+#include<stdio.h>
+int main () {
+
+    int age;
+    printf("Type your age: ");
+    scanf("%d\n", &age);
+
+    if(age > 18) {
+        printf("You are an adult.\n");
+    } else if(age > 12) {
+        printf("You are a teenager.\n");
+    } else if(age > 5) {
+        printf("You are a child.\n");
+    } else {
+        printf("You are a toddler.\n");
+    }
+}
+*/
+
+// Switch statements
+
+/*
+#include <stdio.h>
+int main()
+{
+
+    int day;
+    printf("Enter a number (1-7) for the day of the week: ");
+    scanf("%d", &day);
+
+    switch (day)
+    {
+    case 1:
+        printf("This is Monday.\n");
+        break;
+    case 2:
+        printf("This is Tuesday.\n");
+        break;
+    case 3:
+        printf("This is Wednesday.\n");
+        break;
+    case 4:
+        printf("This is Thursday.\n");
+        break;
+    case 5:
+        printf("This is Friday.\n");
+        break;
+    case 6:
+        printf("This is Saturday.\n");
+        break;
+    case 7:
+        printf("This is Sunday.\n");
+        break;
+
+    default:
+        printf("Invalid input! Please enter a number between 1 and 7.\n");
+        break;
+    }
+    return 0;
+}
+*/
+
+// Ternary Operator
+
