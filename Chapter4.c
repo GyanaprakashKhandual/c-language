d13eac9faad83c103072c6a240858b4c351d25c2
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-01 20:54:55 +0530

    Chapter 4 Running

---
+++ Chapter4.c
@@ -0,0 +1,13 @@
+// Looping in C: The project demonstrates various looping constructs in C.
+
+#include<stdio.h>
+
+int main () {
+
+    int i;
+
+    for ( i=0; i<10; i++) {
+        printf("The value of i is: %d\n", i);
+        
+    }
+}
